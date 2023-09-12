8a96ce9f9854851ce463de976d4a60b4af65eb2e
Mati <65136286+mateyyyy@users.noreply.github.com>
2023-09-12 14:37:09 -0300

    Prompt agregado en vencimiento

--- src/componentes/agregarTarea.js
+++ src/componentes/agregarTarea.js
@@ -1,14 +1,16 @@
 const prompt = require('prompt-sync')();
 function agregarTarea(listaTareas) {
+
     listaTareas[listaTareas.length] = {
         titulo: prompt('Ingrese el titulo de la tarea : '),
         descripcion: prompt('Ingrese la descripcion : '),
         estado: prompt('Ingrese el estado : '),
         creacion: new Date(),
         ultimaEdicion: "",
-        vencimiento: "",
+        vencimiento: new Date(prompt("Ingrese la fecha de vencimiento AAAA-MM-DD : ")),
         dificultad: prompt('Ingrese la dificultad : '),
     };
+
 }
 
 module.exports = agregarTarea;
